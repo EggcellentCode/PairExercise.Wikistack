6b624c64a9f648377e6ab34c0e5af95adb4fc411
Daniel Kim <dankim440@gmail.com>
2022-08-10 23:14:56 +0000

    hooks

--- models/index.js
+++ models/index.js
@@ -2,6 +2,7 @@ const Sequelize = require('sequelize');
 const db = new Sequelize('postgres://localhost/wikistack', {
     logging: false
 });
+//const page = require('../routes/wiki');
 
 const Page = db.define('page', {
     title: {type: Sequelize.STRING, 
@@ -13,7 +14,7 @@ const Page = db.define('page', {
     },
     
     content: {type: Sequelize.TEXT,
-                allowNull: false
+            allowNull: false
     },
     
     status: {type: Sequelize.ENUM('open', 'closed')}
@@ -29,11 +30,21 @@ const User = db.define('user', {
     }
 });
 
-Page.beforeValidate((title) => {
-  Page.slug = title.toString().replace(/\s+/g, '_').replace(/\W/g, '');
+function generateSlug (title) {
   // Removes all non-alphanumeric characters from title
   // And make whitespace underscore
-  return Page.slug;
+  return title.replace(/\s+/g, '_').replace(/\W/g, '');
+}
+
+Page.beforeValidate((Page) => {
+//   const page = Page.findAll({
+//     where: {title: title}  
+//   });   
+//   page.slug = title.toString().replace(/\s+/g, '_').replace(/\W/g, '');
+  // Removes all non-alphanumeric characters from title
+  // And make whitespace underscore
+  // return Page.slug;
+  Page.slug = generateSlug(Page.title);
 });
 
 module.exports = {
